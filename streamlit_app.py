3d23ec6dfae657dc5c383669d77f569f075bbc82
LowriC02 <116714662+LowriC02@users.noreply.github.com>
2024-08-09 21:09:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Adding a tropical background image (hosted online or in your local directory)
-st.image("https://example.com/tropical-background.jpg", use_column_width=True)
+st.image("https://r.search.yahoo.com/_ylt=AwrNPKkwd7Zmfb8y2VpNBQx.;_ylu=c2VjA3NyBHNsawNpbWcEb2lkAzA0NmEyOGVmZThiMGEwYzgzN2NhMzUzZWE3ZDE3NTUxBGdwb3MDMQRpdANiaW5n/RV=2/RE=1723262896/RO=11/RU=https%3a%2f%2fwww.pinterest.com%2fpin%2f679973243720371474%2f/RK=2/RS=QMI2P4TrVr1IHF4io.kSMBXoyYI-", use_column_width=True)
 
 # Engaging App Introduction
 st.title("Welcome to the Cloud Adventure: Your CSP Suggestion Tool!")
